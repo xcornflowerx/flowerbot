d1d4213d13efcf43a269870cf741aa0c2363476d
xcornflowerx <xcornflowerx@gmail.com>
2020-10-23 07:14:07 -0400

    Added shiny status to Pokemon caught in Flowermons
    Signed-off-by: xcornflowerx <xcornflowerx@gmail.com>

--- flowerbot.py
+++ flowerbot.py
@@ -155,7 +155,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         port = 6667
 
         # Create IRC bot connection
-        print('Connecting to %s on port %s...' % (server, port), file = OUTPUT_FILE) 
+        print('Connecting to %s on port %s...' % (server, port), file = OUTPUT_FILE)
         irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:'+ self.token)], self.channel_display_name, self.bot_username)
 
     def init_auto_shoutout_users(self, auto_shoutout_users_filename):
@@ -190,9 +190,18 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
                 for line in flowermons_user_data_file.readlines():
                     data = list(map(lambda x: x.strip().lower(), line.split('\t')))
                     username = data[0]
-                    user_pokemon_set = FLOWERMONS_USER_POKEDEX.get(username, set())
+                    user_pokemon_stats = FLOWERMONS_USER_POKEDEX.get(username, {})
+
+                    user_pokemon_set = user_pokemon_stats.get('CAUGHT', set())
+                    user_pokemon_shiny_set = user_pokemon_stats.get('SHINY', set())
+
                     user_pokemon_set.add(data[1])
-                    FLOWERMONS_USER_POKEDEX[username] = user_pokemon_set
+                    if 'SHINY' in line:
+                        user_pokemon_shiny_set.add(data[1])
+                    user_pokemon_stats['CAUGHT'] = user_pokemon_set
+                    user_pokemon_stats['SHINY'] = user_pokemon_shiny_set
+
+                    FLOWERMONS_USER_POKEDEX[username] = user_pokemon_stats
 
     def on_welcome(self, c, e):
         ''' Handle welcome. '''
@@ -494,10 +503,33 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
     # ---------------------------------------------------------------------------------------------
     # FLOWERMONS
 
+    def determine_shiny_status(self, user_is_sub):
+        '''
+            Determines the shiny status of a caught pokemon assuming full odds (Masuda) is 1/4096.
+            Subscribers have a 1/512 chance of catching a shiny.
+        '''
+        shiny_index = random.randint(0, 4095) # CoUnTiNg StArTs At ZeRo
+        if user_is_sub:
+            shiny_index_lower = (0 if ((shiny_index - 256) < 0) else (shiny_index - 256))
+            shiny_index_upper = (4095 if ((shiny_index + 256) > 4095) else (shiny_index + 256))
+            user_index = random.randint(shiny_index_lower, shiny_index_upper)
+        else:
+            user_index = random.randint(0, 4095)
+        return (user_index == shiny_index)
+
     def format_flowerdex_check_message(self, cmd_issuer, user_is_sub):
-        caught_mons = FLOWERMONS_USER_POKEDEX.get(cmd_issuer, set())
-        if len(caught_mons) > 0:
-            message = '@%s your FlowerDex is %s%% complete and you have %s Flowerballs left!' % (cmd_issuer, self.calculate_flowerdex_completion(cmd_issuer), self.get_users_pokeball_count(cmd_issuer, user_is_sub))
+        user_pokemon_stats = FLOWERMONS_USER_POKEDEX.get(cmd_issuer, {})
+        user_pokemon_set = user_pokemon_stats.get('CAUGHT', set())
+        user_pokemon_shiny_set = user_pokemon_stats.get('SHINY', set())
+
+        if len(user_pokemon_set) > 0:
+            message = '@%s your FlowerDex is %s%% complete' % (cmd_issuer, self.calculate_flowerdex_completion(cmd_issuer))
+            if len(user_pokemon_shiny_set) > 0:
+                if len(user_pokemon_shiny_set) == 1:
+                    message = message + ' (%s shiny caught!) ' % (len(user_pokemon_shiny_set))
+                else:
+                    message = message + ' (%s shinies caught!) ' % (len(user_pokemon_shiny_set))
+            message = message + ' and you have %s Flowerballs left!' % (self.get_users_pokeball_count(cmd_issuer, user_is_sub))
         else:
             message = '@%s you have not caught any pokemon yet :(' % (cmd_issuer)
         return message
@@ -508,7 +540,8 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
         return
 
     def calculate_flowerdex_completion(self, cmd_issuer):
-        caught_mons = FLOWERMONS_USER_POKEDEX.get(cmd_issuer, set())
+        user_pokemon_stats = FLOWERMONS_USER_POKEDEX.get(cmd_issuer, {})
+        caught_mons = user_pokemon_stats.get('CAUGHT', set())
         return round((100 * len(caught_mons) / len(FLOWERMONS_POKEDEX)), 1)
 
     def catch_flowermon(self, cmd_issuer, user_is_sub):
@@ -519,6 +552,7 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             c.privmsg(self.channel, '@%s, you do not have any flowerballs left! BibleThump' % (cmd_issuer))
             return
         pokemon = random.choice(list(FLOWERMONS_POKEDEX))
+        shiny_status = self.determine_shiny_status(user_is_sub)
         self.store_caught_pokemon(cmd_issuer, pokemon)
 
         FLOWERMONS_USER_POKEBALLS[cmd_issuer] = pokeballs - 1
@@ -532,19 +566,30 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
             c.privmsg(self.channel, 'whoops BibleThump @%s broke me snowpoNK' % (cmd_issuer))
         return
 
-    def store_caught_pokemon(self, cmd_issuer, pokemon):
+    def store_caught_pokemon(self, cmd_issuer, pokemon, shiny_status):
         ''' Stores pokemon in data file for user. '''
-        user_pokemon_set = FLOWERMONS_USER_POKEDEX.get(cmd_issuer, set())
+        user_pokemon_stats = FLOWERMONS_USER_POKEDEX.get(cmd_issuer, {})
+        user_pokemon_set = user_pokemon_stats.get('CAUGHT', set())
+        user_pokemon_shiny_set = user_pokemon_stats.get('SHINY', set())
+
         user_pokemon_set.add(pokemon)
-        FLOWERMONS_USER_POKEDEX[cmd_issuer] = user_pokemon_set
+        if shiny_status:
+            user_pokemon_shiny_set.add(pokemon)
+
+        user_pokemon_stats['CAUGHT'] = user_pokemon_set
+        user_pokemon_stats['SHINY'] = user_pokemon_shiny_set
+        FLOWERMONS_USER_POKEDEX[cmd_issuer] = user_pokemon_stats
         self.update_flowermons_user_pokedex_data_file()
 
     def update_flowermons_user_pokedex_data_file(self):
         ''' Updates Flowermons user data file. '''
         with open(self.flowermons_user_data_filename, 'w') as flowermons_user_data_file:
-            for user, user_pokemon_set in FLOWERMONS_USER_POKEDEX.items():
+            for user, user_pokemon_stats in FLOWERMONS_USER_POKEDEX.items():
+                caught_pokemon = user_pokemon_stats['CAUGHT']
+                shiny_pokemon = user_pokemon_stats['SHINY']
                 for pokemon in user_pokemon_set:
-                    flowermons_user_data_file.write('%s\t%s\n' % (user, pokemon))
+                    shiny_status = ('SHINY' if pokemon in shiny_pokemon else '')
+                    flowermons_user_data_file.write('%s\t%s\t%s\n' % (user, pokemon, shiny_status))
 
     def get_users_pokeball_count(self, cmd_issuer, user_is_sub):
         ''' Returns number of pokeballs user has left. '''
@@ -672,8 +717,8 @@ class TwitchBot(irc.bot.SingleServerIRCBot):
                 elif cmd == 'catch':
                     self.catch_flowermon(cmd_issuer, user_is_sub)
         elif cmd == 'queueinit':
-        	if cmd_issuer == self.channel_display_name and len(cmd_args) > 0:
-	            self.init_new_queue_list(cmd_args)
+            if cmd_issuer == self.channel_display_name and len(cmd_args) > 0:
+                self.init_new_queue_list(cmd_args)
         elif cmd == 'deathadd' and user_has_mod_privileges:
             self.death_count += 1
             c.privmsg(self.channel, "%s's current death count is now %s BibleThump" % (self.channel_display_name, self.death_count))
